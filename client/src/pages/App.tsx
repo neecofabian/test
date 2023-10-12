bc2c108d4ced60647218f6e5ba7f192944e41d67
Steven Mathew <ste.tho.mat@gmail.com>
2023-10-11 21:21:46 -0400

    remove vite logo

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -14,8 +14,6 @@ export function App() {
           <div className="flex flex-col items-center gap-y-4">
             <div className="inline-flex items-center gap-x-4">
               <img src={ReactSVG} alt="React Logo" className="w-32" />
-              <span className="text-6xl">+</span>
-              <img src="/vite.svg" alt="Vite Logo" className="w-32" />
             </div>
             <CountButton />
           </div>
