878e5f6c44c901514bcd315607d574a49496e5b8
Neeco Fabian <neecofabian@Neecos-MacBook-Air.local>
2023-10-12 00:56:25 -0400

    feat: route to gallery, create

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import { Routes, Route, Outlet, Link, BrowserRouter } from 'react-router-dom';
 
 import ReactSVG from '@/assets/react.svg';
 import { CountButton } from '@/components/CountButton';
 import { ThemeProvider } from '@/components/ThemeProvider';
 
 import { Page } from '@/components/Page';
+import { Create } from '@/pages/Create';
 
 export function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Gallery />}>
+          <Route path="create" element={<Create />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+// TODO: make root / a Layout component for shared nav bar
+
+function Gallery() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
@@ -19,6 +36,18 @@ export function App() {
           </div>
         </Page>
       </ThemeProvider>
+      <Outlet /> {/* Renders child routes */}
     </main>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the gallery</Link>
+      </p>
+    </div>
   );
 }
---
+++ client/src/pages/Create/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Create() {
+  return (
+    <div>
+      <h2>Create</h2>
+    </div>
+  );
+}
